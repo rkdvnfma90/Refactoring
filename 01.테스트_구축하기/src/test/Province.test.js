23939c4a9f3a87c50c9c800982a1266ef5e16163
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-04-05 18:58:55 +0900

    test 경계 조건 테스트

--- 01.테스트_구축하기/src/test/Province.test.js
+++ 01.테스트_구축하기/src/test/Province.test.js
@@ -20,4 +20,44 @@ describe('Province 테스트', () => {
     expect(korea.shortfall).toBe(-6)
     expect(korea.profit).toBe(292)
   })
+
+  it('zero demand', () => {
+    korea.demand = 0
+    expect(korea.shortfall).toBe(-25)
+    expect(korea.profit).toBe(0)
+  })
+
+  it('negative demand', () => {
+    korea.demand = -1
+    expect(korea.shortfall).toBe(-26)
+    expect(korea.profit).toBe(-10)
+  })
+
+  it('empty string demand', () => {
+    korea.demand = ''
+    expect(korea.shortfall).toBeNaN()
+    expect(korea.profit).toBeNaN()
+  })
+})
+
+describe('No producers 테스트', () => {
+  let noProducers
+  beforeEach(() => {
+    const data = {
+      name: 'No producers',
+      producers: [],
+      demand: 30,
+      price: 20,
+    }
+
+    noProducers = new Province(data)
+  })
+
+  it('shortfall', () => {
+    expect(noProducers.shortfall).toBe(30)
+  })
+
+  it('profit', () => {
+    expect(noProducers.profit).toBe(0)
+  })
 })
