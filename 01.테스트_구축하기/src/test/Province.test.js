f3832127655e956ea189209ef54f313074445b32
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-04-05 18:43:39 +0900

    test profit 테스트

--- 01.테스트_구축하기/src/test/Province.test.js
+++ 01.테스트_구축하기/src/test/Province.test.js
@@ -2,8 +2,16 @@ import Province from '../Province.js'
 import { sampleProvinceData } from '../util/data.js'
 
 describe('Province 테스트', () => {
+  let korea
+  beforeEach(() => {
+    korea = new Province(sampleProvinceData()) // 픽스처 설정
+  })
+
   it('shortfall', () => {
-    const korea = new Province(sampleProvinceData()) // 픽스처 설정
     expect(korea.shortfall).toBe(5) // 검증
+  })
+
+  it('profit', () => {
+    expect(korea.profit).toBe(230) // 검증
   })
 })
